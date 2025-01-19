e7b9820bd565c29c078f92bbd15596f1f557cc27
Jake Styles <jakegstyles@gmail.com>
2025-01-18 21:48:47 -0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Card from "./assets/components/Card"
+
+function App() {
+  return (
+    <div className="flex flex-col relative justify-center items-center h-screen bg-gradient-to-t from-violet-500 to-amber-400">
+      <Card/>
+    </div>
+  )
+}
+
+export default App
