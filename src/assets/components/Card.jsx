2ae503687e105883f8777ae5cf71cf427181b99a
Jake Styles <jakegstyles@gmail.com>
2025-01-18 22:03:30 -0800

    fourth commit

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -2,11 +2,11 @@ import React from "react"
 
 function Card() {
   return (
-    <div className="absolute flex flex-col items-center bg-violet-500 h-full w-full lg:h-96 lg:w-64 scale-75 md:scale-150 rounded-2xl drop-shadow-2xl">
+    <div className="absolute flex flex-col justify-center items-center bg-violet-500 h-screen lg:h-96 lg:w-64 scale-75 md:scale-150 rounded-2xl drop-shadow-2xl">
       <img 
         src="/assets/silverware_hero.png" 
         alt=""
-        className="pt-3" 
+        className="relative pt-3" 
       />
       <a 
         href="https://open.spotify.com/artist/67y3Exu2lTpy9Vp4evxHEB" 
@@ -29,7 +29,7 @@ function Card() {
       <a 
         href="https://www.facebook.com/profile.php?id=100092845976787" 
         target="_blank"
-        className="relative pb-8 text-amber-400 transform -rotate-6 md:rotate-0 [text-shadow:_1px_1px_0_rgb(213_54_0_/_100%),_3px_2px_0_rgb(0_0_0_/_100%)] font-bold hover:text-orange-500 text-4xl md:text-2xl transition-all duration-200"
+        className="relative pb-16 text-amber-400 transform -rotate-6 md:rotate-0 [text-shadow:_1px_1px_0_rgb(213_54_0_/_100%),_3px_2px_0_rgb(0_0_0_/_100%)] font-bold hover:text-orange-500 text-4xl md:text-2xl transition-all duration-200"
       >
         FACEBOOK
       </a>
